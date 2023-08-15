59697ce9d4ae31585b6c07d018817b9b75a2be85
BableshAAzad <bableshaazad@outlook.com>
2023-08-15 09:56:55 +0530

    zigjaz mixed 2 strings

---
+++ Arrayyy/Arrage2ArInZigZagOrder.java
@@ -0,0 +1,60 @@
+package Arrayyy;
+
+import java.util.Scanner;
+
+public class Arrage2ArInZigZagOrder {
+    public static void main(String[] args) {
+        Arrage2ArInZigZagOrder ao = new Arrage2ArInZigZagOrder();
+        System.out.println("Enter the size of 1st first : ");
+        int[] x = ao.readArrayy();
+        System.out.println("Enter the size of 2nd array : ");
+        int[] y = ao.readArrayy();
+        System.out.println("User enter first array : ");
+        ao.display(x);
+        System.out.println("User enter second array : ");
+        ao.display(y);
+        int[] z = ao.zigzag(x, y);
+        System.out.println("After zigzag merged : ");
+        ao.display(z);
+    }
+
+    int[] readArrayy() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] ar = new int[n];
+        System.out.println("Enter " + n + " values");
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = sc.nextInt();
+        }
+        return ar;
+    }
+
+    void display(int[] ar) {
+        for (int i = 0; i < ar.length; i++) {
+            System.out.println(ar[i] + " ");
+        }
+    }
+
+    int[] zigzag(int[] a, int[] b) {
+        int[] c = new int[a.length + b.length];
+        int i = 0, j = 0;
+        while (i < a.length && i < b.length) {
+            c[j] = a[i];
+            j++;
+            c[j] = b[i];
+            i++;
+            j++;
+        }
+        while (i < a.length) {
+            c[j] = a[i];
+            j++;
+            i++;
+        }
+        while (i < b.length) {
+            c[j] = b[i];
+            j++;
+            i++;
+        }
+        return c;
+    }
+}
---
+++ Arrayyy/Mixed2arrSmalltoBigOrder.java
@@ -0,0 +1,63 @@
+package Arrayyy;
+
+import java.util.Scanner;
+
+public class Mixed2arrSmalltoBigOrder {
+    public static void main(String[] args) {
+        Mixed2arrSmalltoBigOrder ao = new Mixed2arrSmalltoBigOrder();
+        System.out.println("Enter the size of 1st first : ");
+        int[] x = ao.readArraysy();
+        System.out.println("Enter the size of 2nd array : ");
+        int[] y = ao.readArraysy();
+        System.out.println("User enter first array : ");
+        ao.displ(x);
+        System.out.println("User enter second array : ");
+        ao.displ(y);
+        int[] z = ao.smallToBig(x, y);
+        System.out.println("After arrange small to big : ");
+        ao.displ(z);
+    }
+     int[] readArraysy() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] ar = new int[n];
+        System.out.println("Enter " + n + " values");
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = sc.nextInt();
+        }
+        return ar;
+    }
+
+    void displ(int[] ar) {
+        for (int i = 0; i < ar.length; i++) {
+            System.out.println(ar[i] + " ");
+        }
+    }
+    int[] smallToBig(int[] a, int[] b) {
+        int[] c = new int[a.length + b.length];
+        int i = 0, j = 0, k=0;
+        while (i < a.length && j < b.length) {
+            if(a[i]<b[j]){
+                c[k]=a[i];
+                k++;
+                i++;
+            }
+            else{
+                c[k]=b[j];
+                k++;
+                j++;
+            }
+        }
+        while (i < a.length) {
+            c[k] = a[i];
+            k++;
+            i++;
+        }
+        while (j < b.length) {
+            c[k] = b[j];
+            k++;
+            j++;
+        }
+        return c;
+    }
+}
