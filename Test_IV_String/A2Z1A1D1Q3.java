151828397e629488bb4ac5e1536026e18ec35dee
BableshAAzad <bableshaazad@outlook.com>
2023-08-29 20:45:39 +0530

    java testIII q

---
+++ Test_IV_String/A2Z1A1D1Q3.java
@@ -0,0 +1,35 @@
+package Test_IV_String;
+
+import java.util.Scanner;
+
+public class A2Z1A1D1Q3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String : ");
+        String str = sc.nextLine();
+        char[] c = str.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            char ch = c[i];
+            int count = 1;
+            int f = i, n = i + 1;
+            if (ch >= 'A' && ch <= 'Z') {
+                if (f == c.length - 1) {
+                    System.out.print(c[f]);
+                    System.out.print("1");
+                    break;
+                }
+                while (c[f] == c[n]) {
+                    count++;
+                    f++;
+                    n++;
+                    i++;
+                    if (f == c.length - 1)
+                        break;
+                }
+                System.out.print(c[f]);
+                System.out.print(count);
+            }
+        }
+        sc.close();
+    }
+}
---
+++ Test_IV_String/PercentageOfCharInWordsQ6.java
@@ -0,0 +1,35 @@
+package Test_IV_String;
+
+import java.util.Scanner;
+
+public class PercentageOfCharInWordsQ6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a word : ");
+        String str = sc.nextLine();
+        perc(str);
+        sc.close();
+    }
+
+    static void perc(String str) {
+        char[] c = str.toCharArray();
+        int[] count = new int[26];
+        for (int i = 0; i < c.length; i++) {
+            char ch = c[i];
+            if (ch >= 'A' && ch <= 'Z')
+                count[ch - 65]++;
+        }
+        int sum = 0;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0)
+                sum += count[i];
+        }
+        double percent = 0.0;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                percent = ((double) count[i] / sum) * 100;
+                System.out.println((char) (i + 65) + " = " + (int) percent + "%");
+            }
+        }
+    }
+}
