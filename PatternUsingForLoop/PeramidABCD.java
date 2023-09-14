8d12c821d2dcb8ac689d640a15b2222b001bde3d
BableshAAzad <bableshaazad@outlook.com>
2023-09-14 23:10:55 +0530

    patter practice java

---
+++ PatternUsingForLoop/PeramidABCD.java
@@ -0,0 +1,18 @@
+package PatternUsingForLoop;
+
+public class PeramidABCD {
+    public static void main(String[] args) {
+        int n = 5, b = 1, a = n;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= a; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= b; j++) {
+                System.out.print((char) (64 + j) + " ");
+            }
+            a--;
+            b = b + 2;
+            System.out.println();
+        }
+    }
+}
---
+++ PatternUsingForLoop/PiramindDown.java
@@ -0,0 +1,18 @@
+package PatternUsingForLoop;
+
+public class PiramindDown {
+    public static void main(String[] args) {
+        int n = 9, space = 0, star = 9;
+        for (int i = 1; i <= (n / 2) + 1; i++) {
+            for (int j = 1; j <= space; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= star; j++) {
+                System.out.print("* ");
+            }
+            space++;
+            star = star - 2;
+            System.out.println();
+        }
+    }
+}
