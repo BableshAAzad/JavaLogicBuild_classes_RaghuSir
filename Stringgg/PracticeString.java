23aff78c01a2f386a9673a56f26a67a0f207b06f
BableshAAzad <bableshaazad@outlook.com>
2023-09-17 23:33:52 +0530

    demo

--- Stringgg/PracticeString.java
+++ Stringgg/PracticeString.java
@@ -1,9 +1,18 @@
 package Stringgg;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PracticeString {
     public static void main(String[] args) {
-        // while(true){
-        //     System.out.println("hello");
-        // }
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        System.out.println("Eneter 5 values : ");
+        for(int i=0; i<5; i++){
+            int a = sc.nextInt();
+            al.add(i, a);
+        }
+        System.out.println(al);
+        System.out.println(al.get(2));
     }
 }
