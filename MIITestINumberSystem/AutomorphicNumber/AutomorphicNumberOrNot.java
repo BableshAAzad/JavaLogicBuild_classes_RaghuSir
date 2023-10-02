534d05d62e86e90d21442cbb0b83dfd25a3078f1
BableshAAzad <bableshaazad@outlook.com>
2023-10-02 19:07:56 +0530

    Automorphic number mII test I

---
+++ MIITestINumberSystem/AutomorphicNumber/AutomorphicNumberOrNot.java
@@ -0,0 +1,27 @@
+package MIITestINumberSystem.AutomorphicNumber;
+
+import java.util.Scanner;
+
+// eg. 5 = 5*5 = 25 last digits are match 5
+// eg. 76 = 76*76 = 5726 last digits are match 76
+// eg. 25 = 25*25 = 625 last digits are match 25
+public class AutomorphicNumberOrNot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a integer number : ");
+        int n = sc.nextInt();
+        System.out.println(isAutomorphic(n) ? "Automorphic" : "Not Automorphic");
+        sc.close();
+    }
+
+    static boolean isAutomorphic(int n) {
+        int pow = n * n;
+        while (n > 0) {
+            if (n % 10 != pow % 10)
+                return false;
+            n = n / 10;
+            pow = pow / 10;
+        }
+        return true;
+    }
+}
