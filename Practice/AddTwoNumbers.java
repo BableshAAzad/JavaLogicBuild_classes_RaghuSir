ee91ddd867ac2bc813834d72ad4aace8bf69d725
BableshAAzad <bableshaazad@outlook.com>
2023-10-27 21:12:07 +0530

    practice java

---
+++ Practice/AddTwoNumbers.java
@@ -0,0 +1,22 @@
+package Practice;
+
+import java.util.Scanner;
+
+class AddTwoNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows : ");
+        int t = sc.nextInt();
+        if (t >= 1 && t <= 1000) {
+            int a, b, sum;
+            for (int i = 0; i < t; i++) {
+                a = sc.nextInt();
+                b = sc.nextInt();
+                if (a >= 0 && b <= 1000) {
+                    sum = a + b;
+                    System.out.println(sum);
+                }
+            }
+        }
+    }
+}
