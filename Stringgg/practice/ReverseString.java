18162c620da1ceaef0f151ea24fd7247adbf6dc4
bableshaazad <bableshaazad@outllok.com>
2024-09-27 15:34:43 +0530

    update str rev 🤌

--- Stringgg/practice/ReverseString.java
+++ Stringgg/practice/ReverseString.java
@@ -1,20 +1,22 @@
+package Stringgg.practice;
 
-import java.utill.*;
+import java.util.Scanner;
 
-public class ReverseString{
-  public static void main(String [] args){
+public class ReverseString {
+  public static void main(String[] args) {
     System.out.println("Enter a String : ");
     Scanner sc = new Scanner(System.in);
     String str = sc.nextLine();
+    sc.close();
     String rev = "";
-    for(int i = 0; i<str.length(); i++){
-        str = str.charAt(i) + str;
+    for (int i = 0; i < str.length(); i++) {
+      rev = str.charAt(i) + rev;
     }
-    System.out.println(rev);
-    if(str.equals(rev)){
-        System.out.println(true)
-    }else{
-        System.out.println(false)
+    if (str.equals(rev)) {
+      System.out.println(true);
+    } else {
+      System.out.println(false);
+      System.out.println(rev);
     }
-  } 
+  }
 }
